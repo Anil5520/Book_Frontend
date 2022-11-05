7e280d1f72314881cbd6c20a2af909b176aaee85
Anil5520 <anilmahto812@gmail.com>
2022-11-05 23:37:50 +0530

    now can delete book

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -1,10 +1,19 @@
 import { Button } from '@mui/material';
+import axios from "axios";
 import React from 'react';
 import './Book.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Book = (props) => {
+    const history = useNavigate();
     const { _id, name, author, price, description, image } = props.book;
+
+    const deleteHandler = async () => {
+        await axios.delete(`http://localhost:5000/books/${_id}`).then((res) => res.data)
+            .then(() => history("/"))
+            .then(() => history("/books"));
+    };
+
     return (
         <div className='card'>
             <img src={image} alt={name} />
@@ -13,9 +22,9 @@ const Book = (props) => {
             <p>{description}</p>
             <h3>Rs {price}</h3>
             <Button LinkComponent={Link} to={`/books/${_id}`} sx={{ mt: 'auto' }}>Update</Button>
-            <Button sx={{ mt: 'auto' }}>Delete</Button>
+            <Button onClick={deleteHandler} sx={{ mt: 'auto' }}>Delete</Button>
         </div>
     )
 }
 
-export default Book
+export default Book;
